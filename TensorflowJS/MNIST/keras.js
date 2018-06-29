ab2238016fe673c3e500eb5b6cb8c3609b85cf42
Bravin Marc <m.bravin@network41.com>
2018-06-29 15:48:10 +0200

    MNIST example

--- TensorflowJS/MNIST/keras.js
+++ TensorflowJS/MNIST/keras.js
@@ -1,22 +1,56 @@
 const IMAGE_SIZE = 28;
 
-const mnist = tf.loadModel("http://localhost:63342/Python/TensorflowJS/MNIST/tfjsmodel/model.json").then(function (model) {
+let mnistModel;
+const mnist = tf.loadModel("http://localhost:63342/DeepLearningPlayground/TensorflowJS/MNIST/tfjsmodel/model.json").then(function (model) {
+    mnistModel = model;
     const image = document.getElementById('img');
-    tensor = convertToTensor(image);
-    console.log(tensor);
-    model.predict(tensor).print();
+    predictImage(image);
 });
+
+function predictImage(img) {
+    tensor = convertToTensor(img);
+    //console.log(tensor);
+    let result = mnistModel.predict(tensor);
+    result.print();
+    //tf.argMax(result).print();
+}
 
 function convertToTensor(img) {
     if (!(img instanceof tf.Tensor)) {
         img = tf.fromPixels(img, 1);
     }
-    var normalized = img.toFloat();
-    var resized = normalized;
+    let normalized = img.toFloat();
+    let resized = normalized;
     if (img.shape[0] !== IMAGE_SIZE || img.shape[1] !== IMAGE_SIZE) {
-        var alignCorners = true;
+        const alignCorners = true;
         resized = tf.image.resizeBilinear(normalized, [IMAGE_SIZE, IMAGE_SIZE], alignCorners);
     }
-    var batched = resized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 1]);
+    let batched = resized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 1]);
     return batched;
 }
+
+function readUrl(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+
+            $("#img").attr("src", e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function(){
+    $("#imageUpload").change(function () {
+     let imageUpload = document.getElementById("imageUpload");
+     if (imageUpload.files.length > 0) {
+         $("#txtSelectedFile").val(imageUpload.files[0].name);
+     }
+     readUrl(this);
+    });
+
+    $("#img").on("load", function(){
+        console.log("image loaded");
+       predictImage(document.getElementById("img"));
+    });
+});
